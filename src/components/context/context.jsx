ca004349a5ba4d42a0b8b95e133136e233d694ad
AlexanderLauinger <al60@illinois.edu>
2025-03-01 15:01:37 -0600

    Better formatting of AI responses

--- src/components/context/context.jsx
+++ src/components/context/context.jsx
@@ -12,7 +12,22 @@ const ContextProvider = (props) => {
             }
             const response = await run(prompt);
             console.log("ContextProvider: Message sent successfully");
-            return response;
+
+            let responseArray = response.split("**");
+
+            let newArray = "";  // Initialize with empty string
+            for(let i = 0; i < responseArray.length; i++){
+                if (i === 0 || i%2 !== 1) {
+                    newArray += responseArray[i];
+                }
+                else {
+                    newArray += "<b>" + responseArray[i] + "</b>";
+                }
+            }
+            let newArray2 = newArray.split("*").join("</br></br>");
+
+
+            return newArray2;
         } catch (error) {
             console.error("ContextProvider: Error sending message to Gemini:", error);
             throw error; // Re-throw the error so it can be handled by the component
